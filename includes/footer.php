046420d07d67562c31fb374cfd3f18b9beede07a
kanikarajput19 <145035733+kanikarajput19@users.noreply.github.com>
2024-07-15 22:31:12 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2024 Student Activity Management System <a href=""_blank"></a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
